d88a3044bb16d7cd6d7be168a45fa0ad38fc869e
Rakshit Kumar <48801964+astronaut99@users.noreply.github.com>
2021-09-09 00:59:38 +0530

    inital commit

---
+++ two_sum.py
@@ -0,0 +1,12 @@
+arr = [3,5,-4,8,11,1,-1,6]
+target = 10
+
+def twosum(arr,target):
+    nums = {}
+    for num in arr:
+        if target - num in nums:
+            return print([target-num,num])
+        else:
+            nums[num] = True
+    return []
+twosum(arr,target)
